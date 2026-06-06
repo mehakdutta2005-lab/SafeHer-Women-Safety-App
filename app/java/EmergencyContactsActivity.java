211cbeee161908e784956d41597d8416e2b5e0e9
mehakdutta2005-lab <mehakdutta2005@gmail.com>
2026-06-07 02:35:11 +0530

    Create EmergencyContactsActivity.java

---
+++ app/java/EmergencyContactsActivity.java
@@ -0,0 +1,8 @@
+package com.safeher.app;
+
+public class EmergencyContactsActivity {
+
+    public void saveContact(String name, String phone) {
+        System.out.println("Contact Saved: " + name);
+    }
+}
